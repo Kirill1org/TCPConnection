5e8aae83af22f5da7a652e6cab78460ad1a3e45a
Kirill1org <39536848+Kirill1org@users.noreply.github.com>
2020-01-15 07:58:40 +0300

    cleared v1.0

--- app/src/main/java/bonch/dev/tcpconnection/MainActivity.java
+++ app/src/main/java/bonch/dev/tcpconnection/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,16 +30,11 @@ public class MainActivity extends AppCompatActivity {
     private Button sendBtn;
 
     private byte[] dataArray;
+    private byte[] recivedDataArray;
 
-    private static Socket socket;
-    private static OutputStream outputStream;
-    private static DataOutputStream dataOutputStream;
-
-    private static final String IP = "192.168.0.100";
-    private static final int PORT = 5000;
-    private final Random random = new Random();
-
-
+    private final String IP = "192.168.0.102";
+    private final int PORT = 5000;
+    private SecureRandom secureRandom = new SecureRandom();
 
 
     @Override
@@ -41,68 +44,86 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
 
-        setClickListeners();
+        startServer();
+    }
 
+    private void startServer() {
+        Single.fromCallable(() -> startListening())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::toPrintSuccessMessage, Throwable -> Log.e("Start server error:", Throwable.getLocalizedMessage()));
 
+    }
+
+    private void toPrintSuccessMessage(byte[] data) {
+        Toast.makeText(this, Arrays.toString(data), Toast.LENGTH_SHORT).show();
+        Log.e("Server recived data:", Arrays.toString(data));
+    }
+
+    private byte[] startListening() {
+        try (ServerSocket serverSocket = new ServerSocket(9090);
+             Socket socket1 = serverSocket.accept();
+             InputStream inputStream = socket1.getInputStream();
+             DataInputStream dataInputStream = new DataInputStream(inputStream)) {
+
+            int length = dataInputStream.readInt();
+
+            if (length != 0) {
+
+                recivedDataArray = new byte[length];
+                dataInputStream.readFully(recivedDataArray, 0, length);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Listening error");
+        }
+        return recivedDataArray;
     }
 
     private void initData() {
 
-        int length = random.nextInt(100)+1;
+        int length = secureRandom.nextInt(100) + 1;
         dataArray = new byte[length];
-        random.nextBytes(dataArray);
-
-
+        secureRandom.nextBytes(dataArray);
     }
 
-    private void setClickListeners() {
+    private void initViews() {
+        infoTV = findViewById(R.id.InfoTextView);
+        sendBtn = findViewById(R.id.send_btn);
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 initData();
-
-                TCPTask tcpTask = new TCPTask();
+                TCPSendTask tcpTask = new TCPSendTask();
                 tcpTask.execute();
 
             }
         });
-
-
     }
 
-    private void initViews() {
-        infoTV = findViewById(R.id.InfoTextView);
-        sendBtn = findViewById(R.id.send_btn);
-    }
-
-    class TCPTask extends AsyncTask<Void, Void, Void> {
+    class TCPSendTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
-            infoTV.setText("Start connection with "+IP+":"+PORT);
+            infoTV.setText("Start connection with " + IP + ":" + PORT);
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
 
-            try {
-
-                socket = new Socket(IP,PORT);
-                outputStream = socket.getOutputStream();
-                dataOutputStream = new DataOutputStream(outputStream);
-
-
+            try (Socket socket = new Socket(IP, PORT);
+                 OutputStream outputStream = socket.getOutputStream();
+                 DataOutputStream dataOutputStream = new DataOutputStream(outputStream)) {
 
                 dataOutputStream.writeInt(dataArray.length);
-                dataOutputStream.write(dataArray,0,dataArray.length);
+                dataOutputStream.write(dataArray, 0, dataArray.length);
 
-                dataOutputStream.close();
-                outputStream.close();
-                socket.close();
 
             } catch (IOException e) {
                 e.printStackTrace();
+                throw new RuntimeException("TCPSend task error");
             }
 
             return null;
